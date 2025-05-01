90dec13c33a6a60da150dfa51838f5a8484f7cce
Marouene Mrabet <mrabet.marouene@gmail.com>
2025-05-01 09:35:47 -0400

    Task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,8 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let bookList = JSON.stringify(books, null, 4);
+  return res.status(200).send(bookList);
 });
 
 // Get book details based on ISBN
